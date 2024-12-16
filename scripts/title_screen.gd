ce0ee7a811bba44727651367906389873a0ccb73
HardAce <inspectrerowhammer@gmail.com>
2024-12-15 22:23:06 -0500

    started playing with themes and grabbed another set of kenney files

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,11 +1,12 @@
 extends Control
 
-@onready var asteroid_count = $Label2
+@onready var asteroid_count = $aCount
 @onready var slider = $HSlider
 
 func _process(_delta: float) -> void:
 	
-	asteroid_count.text = str(slider.step)
+	asteroid_count.text = str(slider.value)
+	Global.asteroid_count = int(asteroid_count.text)
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
 
